f68a8b078f0e9bfe7ed1dc1e5e0184c2485ff765
yanghelenh <yang.helenh@gmail.com>
2019-11-03 22:58:14 -0500

    updates to runEphysExpt.m, not yet done

--- runEphysExpt.m
+++ runEphysExpt.m
@@ -25,156 +25,235 @@ function runEphysExpt()
     % load constant settings
     [dataDir, exptFnDir, settings] = ephysSettings();
     
-    % set up folder structure for organizing data 
-    % prompts for date directory; start from data directory in
-    %  twoPhotonSettings
-    disp('Select date directory');
-    dateDir = uigetdir(dataDir, 'Select date directory'); 
-    [~, dateDirName, ~] = fileparts(dateDir); % get just name of directory
-    cd(dateDir);
+    % Asks whether this is a new cell
+    newCell = input('New Cell? (y/n): ', 's');
     
-    % creates fly folder in date directory
-    % figure out which fly we're on in the date directory
-    dateDirContents = dir(dateDir);
-    currFlyDirs = dateDirContents(...
-        contains({dateDirContents.name},'fly'));
-    flyNum = length(currFlyDirs) + 1;
-    flyDirName = sprintf('fly%02d',flyNum); % fly folder name 
-    mkdir(flyDirName); % make fly folder
-    cd(flyDirName);
-    flyDirPath = pwd;
-    
-    % request fly metadata
-    flyData = getFlyMetadata(dateDirName, flyDirName);
-    
-    % loop through trials
-    flyDone = 'n'; % boolean for whether fly is complete
-    
-    while (~strcmpi(flyDone, 'y'))
-        % asks whether this is new field of view
-        newFOV = input('New FOV? (y/n): ','s');
+    % NEW CELL
+    if (strcmpi(newCell, 'y'))
+        % set up folder structure for organizing data 
+        % prompts for date directory; start from data directory in
+        %  ephysSettings()
+        disp('Select date directory');
+        dateDirPath = uigetdir(dataDir, 'Select date directory');
+        % get just name of directory
+        [~, dateDirName, ~] = fileparts(dateDirPath); 
+        cd(dateDirPath);
         
-        flyDirContents = dir(flyDirPath);
-        currFOVDirs = flyDirContents(...
-            contains({flyDirContents.name},'fov'));
-        % if new field of view, make new fov folder
-        if (strcmpi(newFOV, 'y'))
-            % works even if this is the first
-            fovNum = length(currFOVDirs) + 1; 
-            fovDirName = sprintf('fov%02d',fovNum); % fov folder name
-            mkdir(fovDirName); % make fov folder
-        else
-            fovNum = length(currFOVDirs);
-            fovDirName = sprintf('fov%02d',fovNum); % fov folder name
-            if (~isfolder(fovDirName))
-                newFOV = 'y'; % change this to yes, this is new FOV
-                disp('This is the first FOV for this fly. Creating FOV folder');
-                fovNum = length(currFOVDirs) + 1; 
-                fovDirName = sprintf('fov%02d',fovNum); % fov folder name
-                mkdir(fovDirName); % make fov folder    
+        % figure out which fly we're on in the date directory
+        dateDirContents = dir(dateDirPath);
+        currFlyDirs = dateDirContents(...
+            contains({dateDirContents.name},'fly'));
+        
+        % Asks whether this is a new fly
+        newFly = input('New Fly? (y/n): ', 's');
+        if (strcmpi(newFly, 'n')) % NOT A NEW FLY 
+            flyNum = length(currFlyDirs);
+            
+            if (flyNum == 0)
+                flyNum = 1;
+                flyDirName = sprintf('fly%02d',flyNum); % fly folder name 
+                mkdir(flyDirName);
+            else
+                flyDirName = sprintf('fly%02d',flyNum); % fly folder name 
             end
+            
+            % go to fly directory
+            cd(flyDirName);
+            flyDirPath = pwd;
+            
+            % load fly metadata from previous cell
+            flyDirContents = dir(flyDirPath);
+            currCellDirs = flyDirContents(...
+                contains({flyDirContents.name}, 'cell'));
+            % path to metadata file of last cell for this fly
+            metaDatPath = [currCellDirs.folder filesep ...
+                currCellDirs.name filesep 'metaDat.mat'];
+            % load fly metadata
+            load(metaDatPath, 'flyData');
+        else % NEW FLY (this is default)
+            flyNum = length(currFlyDirs) + 1;
+            flyDirName = sprintf('fly%02d',flyNum); % fly folder name 
+            
+            % creates fly folder in date directory            
+            mkdir(flyDirName); % make fly folder
+            
+            % go to fly directory
+            cd(flyDirName);
+            flyDirPath = pwd;
+            
+            % request fly metadata
+            flyData = getFlyMetadata(dateDirName, flyDirName);
         end
 
-        % go to field of view folder
-        cd(fovDirName);
-        fovDirPath = pwd;
+        % create new cell folder
+        % existing cells for this fly
+        flyDirContents = dir(flyDirPath);
+        currCellDirs = flyDirContents(...
+            contains({flyDirContents.name}, 'cell'));
+        cellNum = length(currCellDirs) + 1;
+        cellDirName = sprintf('cell%02d', cellNum); % cell folder name
+        % create cell folder
+        mkdir(cellDirName);
         
-        % create new trial folder
-        fovDirContents = dir(fovDirPath);
-        currTrialDirs = fovDirContents(...
-            contains({fovDirContents.name},'trial'));
-        trialNum = length(currTrialDirs) + 1;
-        trialDirName = sprintf('trial%02d',trialNum); % trial folder name
-        mkdir(trialDirName);
-        cd(trialDirName); % go to trial folder
-        trialDirPath = pwd;
+        % go to cell directory
+        cd(cellDirName);
+        cellDirPath = pwd;
         
-        % prompt user to select an experiment
-        exptSelected = 0;
-        disp('Select an experiment');
-        while ~exptSelected
-            exptTypeFileName = uigetfile('*.m', 'Select an experiment',...
-                exptFnDir);
-            % if user cancels or selects valid file
-            if (exptTypeFileName == 0)
-                disp('Selection cancelled');
-                exptSelected = 1; % end loop
-            elseif (contains(exptTypeFileName, '.m'))
-                disp(['Experiment: ' exptTypeFileName]);
-                exptSelected = 1; % end loop
-            else
-                disp('Select an experimental .m file or cancel');
-                exptSelected = 0;
-            end
-        end
-        % if user cancels at this point 
-        if (exptTypeFileName == 0)
-            cd(fovDirPath) % move up out of trial folder into fov folder
-            rmdir(trialDirName); % delete this trial folder
-            % if this was a new field of view, delete that folder as well
-            if (strcmpi(newFOV, 'y'))
-                cd(flyDirPath) % move up out of fov folder into fly folder
-                rmdir(fovDirName)
-            end
-            % ask user whether to end fly
-            flyDone = input('End fly? (y/n): ', 's');
-            if (strcmpi(flyDone, 'y'))
-                disp('Ending fly.');
-            end
-            % restarts loop without running anything (or ends fly 
-            %  completely)
-            continue 
-        end
-        % prompt user for experiment duration
-        exptDuration = input(...
-            'Experiment duration in seconds. Enter 0 to end fly: ');
-        % to end fly at this stage
-        if (exptDuration == 0)
-            disp('Ending fly.');
-            cd(fovDirPath) % move up out of trial folder
-            rmdir(trialDirName); % delete this trial folder
-            % if this was a new field of view, delete that folder as well
-            if (strcmpi(newFOV, 'y'))
-                cd(flyDirPath) % move up out of fov folder
-                rmdir(fovDirName)
-            end
-            flyDone = 'y';
-            % will end looping
-            continue
-        end
+        % **ASKS TO RUN PRE-EXPT ROUTINES**
+ 
+    % NOT A NEW CELL    
+    elseif (strcmpi(newCell, 'n'))
+        % prompt for cell folder
+        disp('Select cell folder');
+        cellDirPath = uigetdir(dataDir, 'Select cell folder');
+        % get cell directory name, fly directory path
+        [flyDirPath, cellDirName, ~] = fileparts(cellDirPath); 
+        % get fly directory name, date path
+        [dateDirPath, flyDirName, ~] = fileparts(flyDirPath);
+        % get date directory name
+        [~, dateDirName, ~] = fileparts(dateDirPath);
         
-        % run experiment
-        % convert selected experiment file into function handle
-        % get name without .m
-        exptTypeName = extractBefore(exptTypeFileName, '.');
-        exptFn = str2func(exptTypeName);
+        % go to cell directory
+        cd(cellDirPath);
         
-        % display this experiment's path
-        disp(pwd);
-        
-        % run actual experiment code
-        try
-            exptReturn = exptFn(settings, flyData, exptDuration); 
-        catch
-            disp('Invalid Experimental Function');
-            % undo folder creation
-            cd(fovDirPath) % move up out of trial folder
-            rmdir(trialDirName); % delete this trial folder
-            % if this was a new field of view, delete that folder as well
-            if (strcmpi(newFOV, 'y'))
-                cd(fovDirPath) % move up out of fov folder
-                rmdir(fovDirName)
-            end
-        end
-        
-        % if experiment ran to completeion without errors
-        if (exptReturn == 1)
-            disp('Experiment complete!');
-        end
-        
-        % prompt user to run another trial or end fly
-        flyDone = input('End fly? (y/n): ','s');
-        
-        cd(flyDirPath);
+        % **CONTINUES WITH NEXT TRIAL**
+    
+    % INVALID INPUT, DON'T DO ANYTHING
+    else
+        disp('Invalid input. Ending runEphysExpt()');
+        return;
     end
+    
+    
+    
+            % loop through trials
+        flyDone = 'n'; % boolean for whether fly is complete
+
+        while (~strcmpi(flyDone, 'y'))
+            % asks whether this is new field of view
+            newFOV = input('New FOV? (y/n): ','s');
+
+            flyDirContents = dir(flyDirPath);
+            currFOVDirs = flyDirContents(...
+                contains({flyDirContents.name},'fov'));
+            % if new field of view, make new fov folder
+            if (strcmpi(newFOV, 'y'))
+                % works even if this is the first
+                fovNum = length(currFOVDirs) + 1; 
+                fovDirName = sprintf('fov%02d',fovNum); % fov folder name
+                mkdir(fovDirName); % make fov folder
+            else
+                fovNum = length(currFOVDirs);
+                fovDirName = sprintf('fov%02d',fovNum); % fov folder name
+                if (~isfolder(fovDirName))
+                    newFOV = 'y'; % change this to yes, this is new FOV
+                    disp('This is the first FOV for this fly. Creating FOV folder');
+                    fovNum = length(currFOVDirs) + 1; 
+                    fovDirName = sprintf('fov%02d',fovNum); % fov folder name
+                    mkdir(fovDirName); % make fov folder    
+                end
+            end
+
+            % go to field of view folder
+            cd(fovDirName);
+            fovDirPath = pwd;
+
+            % create new trial folder
+            fovDirContents = dir(fovDirPath);
+            currTrialDirs = fovDirContents(...
+                contains({fovDirContents.name},'trial'));
+            trialNum = length(currTrialDirs) + 1;
+            trialDirName = sprintf('trial%02d',trialNum); % trial folder name
+            mkdir(trialDirName);
+            cd(trialDirName); % go to trial folder
+            trialDirPath = pwd;
+
+            % prompt user to select an experiment
+            exptSelected = 0;
+            disp('Select an experiment');
+            while ~exptSelected
+                exptTypeFileName = uigetfile('*.m', 'Select an experiment',...
+                    exptFnDir);
+                % if user cancels or selects valid file
+                if (exptTypeFileName == 0)
+                    disp('Selection cancelled');
+                    exptSelected = 1; % end loop
+                elseif (contains(exptTypeFileName, '.m'))
+                    disp(['Experiment: ' exptTypeFileName]);
+                    exptSelected = 1; % end loop
+                else
+                    disp('Select an experimental .m file or cancel');
+                    exptSelected = 0;
+                end
+            end
+            % if user cancels at this point 
+            if (exptTypeFileName == 0)
+                cd(fovDirPath) % move up out of trial folder into fov folder
+                rmdir(trialDirName); % delete this trial folder
+                % if this was a new field of view, delete that folder as well
+                if (strcmpi(newFOV, 'y'))
+                    cd(flyDirPath) % move up out of fov folder into fly folder
+                    rmdir(fovDirName)
+                end
+                % ask user whether to end fly
+                flyDone = input('End fly? (y/n): ', 's');
+                if (strcmpi(flyDone, 'y'))
+                    disp('Ending fly.');
+                end
+                % restarts loop without running anything (or ends fly 
+                %  completely)
+                continue 
+            end
+            % prompt user for experiment duration
+            exptDuration = input(...
+                'Experiment duration in seconds. Enter 0 to end fly: ');
+            % to end fly at this stage
+            if (exptDuration == 0)
+                disp('Ending fly.');
+                cd(fovDirPath) % move up out of trial folder
+                rmdir(trialDirName); % delete this trial folder
+                % if this was a new field of view, delete that folder as well
+                if (strcmpi(newFOV, 'y'))
+                    cd(flyDirPath) % move up out of fov folder
+                    rmdir(fovDirName)
+                end
+                flyDone = 'y';
+                % will end looping
+                continue
+            end
+
+            % run experiment
+            % convert selected experiment file into function handle
+            % get name without .m
+            exptTypeName = extractBefore(exptTypeFileName, '.');
+            exptFn = str2func(exptTypeName);
+
+            % display this experiment's path
+            disp(pwd);
+
+            % run actual experiment code
+            try
+                exptReturn = exptFn(settings, flyData, exptDuration); 
+            catch
+                disp('Invalid Experimental Function');
+                % undo folder creation
+                cd(fovDirPath) % move up out of trial folder
+                rmdir(trialDirName); % delete this trial folder
+                % if this was a new field of view, delete that folder as well
+                if (strcmpi(newFOV, 'y'))
+                    cd(fovDirPath) % move up out of fov folder
+                    rmdir(fovDirName)
+                end
+            end
+
+            % if experiment ran to completeion without errors
+            if (exptReturn == 1)
+                disp('Experiment complete!');
+            end
+
+            % prompt user to run another trial or end fly
+            flyDone = input('End fly? (y/n): ','s');
+
+            cd(flyDirPath);
+        end
 end
