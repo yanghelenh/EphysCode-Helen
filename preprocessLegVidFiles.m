e821fb477d26acb3a69170480c52c8ba6420f077
yanghelenh <yang.helenh@gmail.com>
2020-07-03 13:04:54 -0400

    updates to leg vid preprocessing code

--- preprocessLegVidFiles.m
+++ preprocessLegVidFiles.m
@@ -30,9 +30,65 @@ function preprocessLegVidFiles()
     disp('Select date directory');
     dateDirPath = uigetdir(dataDir, 'Select date directory');
     
-    % get fly folders
+    % contents of date directory
     dateDirContents = dir(dateDirPath);
+    
+    % get fly folders
     flyDirs = dateDirContents(contains({dateDirContents.name},'fly'));
     
+    % create scripts folder if it doesn't exist
+    if (sum(strcmpi({dateDirContents.name},'scripts')))
+        mkdir(dateDirPath, 'scripts');
+    end
+    % full path to scripts folder
+    scriptsPath = [dateDirPath filesep 'scripts'];
     
+    % loop over all fly directories
+    for i = 1:length(flyDirs)
+        flyDirPath = [flyDirs(i).path filesep flyDirs(i).name];
+        
+        % get cell folders
+        flyDirContents = dir(flyDirPath);
+        cellDirs = flyDirContents(contains({flyDirContents.name}, 'cell'));
+        
+        % loop over all cell folders
+        for j = 1:length(cellDirs)
+            cellDirPath = [cellDirs(j).path filesep cellDirs(j).name];
+            
+            % go to cell folder
+            cd(cellDirPath);
+            
+            % get trial files
+            cellDirContents = dir(cellDirPath);
+            trialFiles = cellDirContents(contains({cellDirContents.name}, ...
+                'trial'));
+            
+            % loop over all trials
+            for k = 1:length(trialFiles)
+                trialPath = [trialFiles(k).path filesep trialFiles(k).name];
+                % load inputParams from trial file (has impt metadata)
+                load(trialPath, 'inputParams');
+                      
+                % only execute leg video processing on trials with leg vid
+                if (contains(inputParams.exptCond, 'legVid'))
+                    % raw leg vid folder
+                    rawLegVidPath = [cellDirPath filesep 'rawLegVid'];
+                    
+                    % current number of frames grabbed same as start index
+                    %  for new video with zero indexing
+                    startFrame = inputParams.startLegVidNum;
+                    % total number of frames grabbed for this trial
+                    totFrames = inputParams.endLegVidNum - ...
+                        inputParams.startLegVidNum;
+                    
+                    
+                    
+                    
+                end
+                
+            end
+            
+            
+        end
+    end
 end
